f64c6a7bcb7ed977aaf0c409d44898e89a047efb
Харитонин Иван <i.haritonin@crpt.ru>
2022-04-09 11:50:15 +0300

    test

--- src/main/java/InitCLass.java
+++ src/main/java/InitCLass.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Vector;
+
 public class InitCLass {
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,7};
+        System.out.println(RemoveDupl(nums));
+
+    }
+    public static int RemoveDupl(int[] nums)
+    {
+        ArrayList<Integer> lst =new ArrayList<>();
+        for (int i = 0; i < nums.length-1; i++) {
+                if (nums[i] != nums[i + 1])
+                    lst.add(nums[i]);
+        }
+        lst.add(nums[nums.length-1]);
+        for (int i = 0; i < lst.size(); i++) {
+            nums[i] = lst.get(i);
+        }
+        return lst.size();
+
+    }
 }
---
+++ src/main/java/Merge2ListNodes.java
@@ -0,0 +1,20 @@
+public class Merge2ListNodes {
+    public static void main(String[] args) {
+
+    }
+}
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+//class Solution {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        if(list1.val==list2.val)
+//        {
+//            list1.next
+//        }
+//    }
+//}
